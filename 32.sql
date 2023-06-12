b3389c91be9e0523508a7eeecb4f47513dfa6188
NeeRaj-2401 <niraj.yadav@slrtce.in>
2023-06-13 00:55:33 +0530

    32. One more approach

--- 32.sql
+++ 32.sql
@@ -3,3 +3,10 @@ SELECT e.employee_id, e.first_name, e.last_name
 FROM employees e
 LEFT JOIN orders o ON e.employee_id = o.employee_id
 WHERE o.order_id IS NULL;
+ 
+--  OR 
+SELECT e.Employee_ID, e.First_Name, e.Last_Name
+FROM Employees e
+WHERE e.Employee_ID NOT IN (
+    SELECT DISTINCT Employee_ID FROM Orders
+)
